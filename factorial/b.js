05df8509f6beb0041d50ecf340010f285b829627
abpdf <113277470+abpdf@users.noreply.github.com>
2024-09-17 15:23:33 +0800

    Update b.js

--- factorial/b.js
+++ factorial/b.js
@@ -1,13 +1,9 @@
 self.onmessage = function (data2) {
-    function a1 () {
-        postMessage(i);
-    }
-    setInterval(a1 (), 500);
     num=data2.data;
-    let result = 1n; // 使用BigInt来处理大整数
-    var i;
-    for (i = 2n; i <= BigInt(num); i++) {
+    let result = 1n; // 使用BigInt来处理大整数  
+    for (let i = 2n; i <= BigInt(num); i++) {
         result *= i;
+        postMessage(i);
     }//计算
     postMessage(result);
 }
